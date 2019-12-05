318bf6d6708a10a224b4f9664fdb1cffb34799de
Roel Stappers <roel.stappers@met.no>
2019-12-05 14:49:39 +0100

    Update tests

--- src/Harmonie_namelists.jl
+++ src/Harmonie_namelists.jl
@@ -6,7 +6,7 @@ using OrderedCollections, TOML
 
 # export schema, isvalid, diagnose
 export NAMELIST_DIR
-export dict2namelist, read_namelists, merge_namelists
+export dict2namelist, read_namelists, merge_namelists, replace_env!
 
 const moduledir=@__DIR__ 
 # const schemafile="$moduledir/namelists.schema.json"
@@ -31,6 +31,49 @@ read_namelists(names) = [TOML.parsefile("$NAMELIST_DIR/$name.toml") for name in 
 Returns a dictionary based on an arrays of dicts 
 """
 merge_namelists(dicts) = merge!(merge,OrderedDict{String,Any}(),dicts...) 
+
+
+#In Harmonie we support two patterns for strings in the YAML files
+#    1) a string starting with $ is taken from the  enviroment and errors if the ENV variable is not set
+#    2) a string starting with $ and containing a ? will use the ENV variable or the default value 
+#        after the ? 
+#     e.g. LAT0 : '$LAT0?5' in a YAML file means take LAT0 from the ENVironment or set it equal to 5
+
+""" 
+    replace_val_with_env(val)   
+
+  
+"""
+replace_val_with_env(val::Number)  = val
+replace_val_with_env(val::Bool)  = val
+
+function replace_val_with_env(val::String) 
+    if startswith(val, '$')
+        str = lstrip(val, '$')
+        if occursin("?", str)
+            temp = split(str, '?')
+            key = temp[1]
+            default = temp[2]
+            haskey(ENV, key) ? ENV[key]  : default  
+        else
+            haskey(ENV, str) ? ENV[str]  : error("No Env[$str]")
+        end         
+    else
+        val
+    end
+end
+
+"""
+    replace_env!(d::AbstractDict) 
+
+"""
+function replace_env!(d::AbstractDict) 
+    for (name, namelist) in d
+        for (key,val) in namelist
+            d[name][key] = replace_val_with_env(val)
+        end  
+    end 
+end 
 
 
 # In the future we wil validate namelists using JSONSchema
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,34 @@
 
-import Harmonie_namelists
-import YAML, JSONSchema
-
+using Harmonie_namelists
 using Test
 
-BINDIR="$(pathof(Harmonie_namelists))/../bin"
 
+# These are very basic test. Once we have JSONSchema files for namelists 
+# we could validate the namelists much better
+
+# Check that the mechanism where we  take variables from the ENVironment works
+@testset "ENV" begin 
+    dicts = read_namelists(["global"])
+    totdict = merge_namelists(dicts)
+    # ENV["ENSMBR"] is not defined so replace_env! throws an error
+    @test_throws ErrorException replace_env!(totdict)
+
+    # After setting ENV["ENSMBR"] the same call should succeeds (returns nothing)
+    ENV["ENSMBR"] = 1
+    @test replace_env!(totdict) === nothing
+
+end 
+
+
+BINDIR="../bin"
 @testset "Namelists " begin
-    @test read(`$BINDIR/gen_namelist.jl args`) == 
+    # This should be improved we only test that Gen_namelist produces output
+    @test !isempty(read(`$BINDIR/gen_namelist.jl args`))
+    @test !isempty(read(`$BINDIR/Get_namelist.jl`))
 end
 
-end
+run_with__env(cmd) = run(Cmd(cmd,env=ODBENV))
+
+
+
+
