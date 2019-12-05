bdca936eaa08aa60d286351ffd7017f4100348b9
Roel Stappers <roel.stappers@met.no>
2019-12-05 14:49:23 +0100

    Update bins

--- bin/Get_namelist.jl
+++ bin/Get_namelist.jl
@@ -2,33 +2,43 @@
 
 using Harmonie_namelists
 
+# Usage:
+#    Get_namemlist.jl 
+# Will create the namelists defined in configs (see below) 
+# Currently output is written to stdout
+
 
 # We set ENV here. Could run `export LMPOFF=".TRUE."` etc  before calling this script.  
 ENV["LMPOFF"] = ".TRUE."
 ENV["ENSMBR"] = 1
 ENV["L_GATHERV_WRGP"] = ".TRUE." 
 ENV["NPROMA"]  = 1
+ENV["LHARATU"]  = ".TRUE."
 ENV["NPROC_IO"] = 1
 ENV["CIFDIR"] = "1"
 ENV["TEFRCL"] = 1
+ENV["LSTATNW"] = ".TRUE."
+ENV["NXGSTPERIOD"] = 0
 
 default = ["global", "host_specific", "mpp","file"]
 dynamics = "arome"
 
-# These are not consistent with Get_namelist yet
+# Note: these are not consistent with Get_namelist
 # should read this from yaml/toml file
-configs = Dict("e927" => [default; "dynamics"; "dfi"; "args"],
+configs = Dict("e927" => [default; dynamics; "dfi"; "args"],
+               "forecast" => [default; dynamics;  "surfex"; "args"],
                "e401" => [default; "e401"; "args"],
                "pp"  => [default; "fullpos"; "pp_default"; "pp_surfex"; "pp_arome"  ],    # -r NAMFPC
                "oulan" => ["oulan"],
                "screening" => [default; "screening"; "arome_screening"; "args"],
-               "minimization" => [default; "arome_minimization"])
+               "minimization" => [default; "arome_minimization"],
+               "screen4d" => [default; "screening"; dynamics; "args"])
 
 io =stdout
-for key in keys(configs) 
-    println(key)
+for key in keys(configs)     
     dicts = read_namelists(configs[key])
     totdict = merge_namelists(dicts)
+    replace_env!(totdict)
     dict2namelist(io,totdict)
 
 
--- bin/gen_namelist.jl
+++ bin/gen_namelist.jl
@@ -1,9 +1,15 @@
 #!/usr/bin/env julia
 
+
+# Usage example:
+#      gen_namelist.jl global args  > fort.4
+# will merge global.toml and args.toml and convert to namelist format 
+
 using Harmonie_namelists
 
 io = stdout   # io = open("fort.4")
 
 dicts = read_namelists(ARGS)
 totdict = merge_namelists(dicts)
+replace_env!(totdict)
 dict2namelist(io,totdict)
