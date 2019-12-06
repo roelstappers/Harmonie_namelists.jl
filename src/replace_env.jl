8ca1de1a10b8e05e1e87e79b7b179a318bc31973
Roel Stappers <roel.stappers@met.no>
2019-12-06 15:56:09 +0100

    Remove duplicate TEFRCL

--- src/replace_env.jl
+++ src/replace_env.jl
@@ -1,14 +1,13 @@
-#In Harmonie we support two patterns for strings in the YAML files
-#    1) a string starting with $ is taken from the  enviroment and errors if the ENV variable is not set
-#    2) a string starting with $ and containing a ? will use the ENV variable or the default value 
-#        after the ? 
+#  We support two patterns for strings in the YAML files
+#  1) a string starting with $ is taken from the  enviroment and errors if the ENV variable is not set
+#  2) a string starting with $ and containing a ? will use the ENV variable or the default value after the ? 
 #     e.g. LAT0 : '$LAT0?5' in a YAML file means take LAT0 from the ENVironment or set it equal to 5
 
 
+string2type = Dict("Bool"=> Bool, "String" => String, "Int" => Int64, "Float" => Float64 )
+
 replace_val_with_env(key, val::Number) = val    
 replace_val_with_env(key, val::Bool) = val
-
-string2type = Dict("Bool"=> Bool, "String" => String, "Int" => Int64, "Float" => Float64 )
 
 function replace_val_with_env(key,val::String)     
     if startswith(val, '$')
